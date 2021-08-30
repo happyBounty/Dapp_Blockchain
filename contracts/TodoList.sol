4f92aee341cadc5c2477c63d3c939c4ae69ea5a0
OliveBounty <oliveodobounty@gmail.com>
2021-08-30 22:25:49 +0900

    finish Dapp using html

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,9 +11,27 @@ contract TodoList {
     string content;
     bool completed;
   }
+
+    event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+    event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
   mapping (uint=>Task) public tasks;
+    function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
   function createTask(string memory _content) public {
       taskCount ++;
       tasks[taskCount] = Task(taskCount, _content, false);
+          emit TaskCreated(taskCount, _content, false);
   }
 }
